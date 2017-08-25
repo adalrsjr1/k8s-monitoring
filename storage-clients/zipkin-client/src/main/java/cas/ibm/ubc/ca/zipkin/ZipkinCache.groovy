0a8c3a710b0f9002ea89b20e66a01fb1698ea3b8
Adalberto Junior <adalrsjr1@gmail.com>
2017-08-25 16:06:42 -0700

    zipkin-client should be refactored

--- storage-clients/zipkin-client/src/main/java/cas/ibm/ubc/ca/zipkin/ZipkinCache.groovy
+++ storage-clients/zipkin-client/src/main/java/cas/ibm/ubc/ca/zipkin/ZipkinCache.groovy
@@ -1,13 +1,21 @@
 package cas.ibm.ubc.ca.zipkin
 
+import java.util.concurrent.TimeUnit
+
+import cas.ibm.ubc.ca.zipkin.pogos.BinaryAnnotation
 import cas.ibm.ubc.ca.zipkin.pogos.Trace
 import cas.ibm.ubc.ca.zipkin.util.ZipkinTraceUtil
 
+// http://cloud.spring.io/spring-cloud-static/spring-cloud-sleuth/1.2.0.RELEASE/
 class ZipkinCache {
 	Map<String,List<List<Trace>>> traces
 	
 	// list of messages ordered by ip
 	Map<String, List> messages
+	
+	Map<String, Set> operations = [:]
+	
+	private static ZipkinCache INSTANCE 
 	
 	ZipkinCache(Map<String,List<List<Trace>>> traces) {
 		this.traces = traces
@@ -21,8 +29,16 @@ class ZipkinCache {
 		}		
 	}
 	
+	public static ZipkinCache zipkinCache() {
+		if(!INSTANCE) {
+			ZipkinRequestor requestor = new ZipkinRequestor("192.168.99.100", 30002, 10, TimeUnit.SECONDS)
+			
+			INSTANCE = new ZipkinCache(requestor.getTraces([limit:100000]))
+		}
+	}
+	
 	//TODO: How to get an operation?
-	// Is it an operation an aggregation of several annotations
+	// Is it the operation an aggregation of several annotations
 	// of a trace (message)
 	
 	private Map mapTracesByIp(List<List<Trace>> traces) {
@@ -30,7 +46,10 @@ class ZipkinCache {
 		traces.each { list ->
 			list.each { trace ->
 				String ip = ZipkinTraceUtil.getFirstIP(trace)
-				Map message = [traceId: trace.traceId, id:trace.id, timestamp:trace.timestamp, duration:trace.duration]
+				Map message = [traceId: trace.traceId, id:trace.id,
+					timestamp:trace.timestamp, duration:trace.duration,
+					message:getMessagePath(trace.binaryAnnotations)]
+				
 				if(map.containsKey(ip)) {
 					map[ip] << message
 				}
@@ -39,32 +58,32 @@ class ZipkinCache {
 				}
 			}
 		}
+		
 		return map
 		
+	}
+	
+	private Map getMessagePath(Collection<BinaryAnnotation> annotations) {
+		annotations.inject([:]) { result, annotation ->
+			if(annotation.key ==~ /http.*/ || annotation.key ==~ /error.*/) {
+				result[annotation.key] = annotation.value
+			}
+			result
+		}
 	}
 	
 	List getMessagesByIp(String ip) {
 		messages.getOrDefault(ip, [])
 	}
 	
-	List<String> getOperations(String serviceCaller) {
-		List<List<Trace>> traces = traces[serviceCaller]
+	Set getOperationByServiceName(String ip) {
+		List messages = getMessagesByIp(ip)
 		
-		traces.each{trace ->
-		
-		trace.inject([] as Set) { result, t ->
-			println  t.binaryAnnotations.inject([:]) {map, bAnn ->
-				map["{$t.duration}B::${bAnn.endpoint.ipv4}[${bAnn.endpoint.port}]"]  = bAnn.value
-				map
-			}
-			println  t.annotations.inject([:]) {map, ann ->
-				map["{$t.duration}${ann.timestamp}:${ann.endpoint.ipv4}[${ann.endpoint.port}]"]  = ann.value
-				map
-			}
+		messages.inject([] as Set) { result, message ->
+			result << message.message
 			result
 		}
-		println ""
-		}
+		
 	}
 	
 	
